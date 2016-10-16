b0801d4a88d897bed0ff45614ca4ac856728f5bd
Daniel Lucsanszky <dan.lucsanszky@gmail.com>
2016-10-16 21:37:39 +0200

    serving on localhost

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,18 +4,23 @@
 {-# LANGUAGE TypeOperators #-}
 
 module Lib
-( run
+( runApp
 ) where
 
+import Control.Monad.IO.Class
 import Control.Monad.Trans.Except (ExceptT, runExceptT)
 import Data.Aeson
 import Data.Proxy
 import Data.Text
+import qualified Data.Text.IO as T (getLine)
 import GHC.Generics
-import Network.HTTP.Client (Manager, newManager, defaultManagerSettings)
+import Network.HTTP.Client (Manager, newManager)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Network.Wai
+import Network.Wai.Handler.Warp
 import Servant.API
 import Servant.Client
+import Servant.Server
 
 type GitRepoAPI = "users"
                :> Header "User-Agent" Text
@@ -23,28 +28,48 @@ type GitRepoAPI = "users"
                :> "repos" 
                :> Get '[JSON] [Repo]
 
+type MyAPI = Capture "username" Text 
+          :> Get '[JSON] [Repo]
+
 data Repo = Repo 
   { name :: Text
   , size :: Int
-  } deriving (Show, Generic)
+  } | NotFound 
+      { documentation_url :: Text
+      , message :: Text
+      } deriving (Show, Generic)
 
 instance ToJSON Repo
 instance FromJSON Repo
 
-api :: Proxy GitRepoAPI
-api = Proxy
+gitAPI :: Proxy GitRepoAPI
+gitAPI = Proxy
+
+myAPI :: Proxy MyAPI
+myAPI = Proxy
 
 getRepos :: Maybe Text -> Text
-                 -> Manager 
-                 -> BaseUrl 
-                 -> ExceptT ServantError IO [Repo]
-getRepos = client api
+                       -> Manager 
+                       -> BaseUrl 
+                       -> ExceptT ServantError IO [Repo]
+getRepos = client gitAPI
 
-run :: IO ()
-run = do
-	manager <- newManager tlsManagerSettings
-	result  <- runExceptT $ getRepos (Just "Lucsanszky.github.io") "Lucsanszky" manager $ BaseUrl Https "api.github.com" 443 ""
-	case result of
-		Left err    -> putStrLn $ "Error: " ++ show err
-		Right repos -> do
-			print repos
+server :: Server MyAPI
+server = pingGitHub 
+  where pingGitHub :: Text -> Handler [Repo]
+        pingGitHub user = liftIO $ do
+          manager <- newManager tlsManagerSettings
+          result  <- runExceptT 
+                   $ getRepos (Just "lucsanszky.github.io") user manager 
+                   $ BaseUrl Https "api.github.com" 443 ""
+
+          case result of
+          	Right repos -> return repos
+          	Left err    -> return 
+          	             $ [NotFound "https://developer.github.com/v3" "User Not Found"]
+
+app :: Application
+app = serve myAPI server
+
+runApp :: IO ()
+runApp = run 3333 app
