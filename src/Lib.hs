9d484cf34571db857ae34728af0ebdbdcbdd68d3
Daniel Lucsanszky <dan.lucsanszky@gmail.com>
2016-10-23 00:50:45 +0200

    better error handling, more json data, cleaner code

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,40 +7,49 @@ module Lib
 ( runApp
 ) where
 
-import Control.Monad.IO.Class
+import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Except (ExceptT, runExceptT)
 import Data.Aeson
+import Data.List (sortBy)
 import Data.Proxy
-import Data.Text
-import qualified Data.Text.IO as T (getLine)
+import qualified Data.Text as T
 import GHC.Generics
 import Network.HTTP.Client (Manager, newManager)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Network.Wai
 import Network.Wai.Handler.Warp
+import qualified Servant as S (throwError)
 import Servant.API
 import Servant.Client
 import Servant.Server
 
-type GitRepoAPI = "users"
-               :> Header "User-Agent" Text
-               :> Capture "username" Text 
-               :> "repos" 
-               :> Get '[JSON] [Repo]
-
-type MyAPI = Capture "username" Text 
-          :> Get '[JSON] [Repo]
-
+-- | Data structure for the JSON data
 data Repo = Repo 
-  { name :: Text
-  , size :: Int
-  } | NotFound 
-      { documentation_url :: Text
-      , message :: Text
-      } deriving (Show, Generic)
+  { name             :: T.Text
+  , full_name        :: T.Text
+  , size             :: Int
+  , description      :: Maybe T.Text
+  , language         :: T.Text
+  , created_at       :: T.Text
+  , clone_url        :: T.Text
+  , stargazers_count :: Int
+  } deriving (Show, Generic)
 
 instance ToJSON Repo
 instance FromJSON Repo
+
+-- | Type declaration for describing a GitHub API endpoint
+-- reachable at: /users/:username/repos
+type GitRepoAPI = "users"
+               :> Header "User-Agent" T.Text
+               :> Capture "username" T.Text 
+               :> "repos" 
+               :> Get '[JSON] [Repo]
+
+-- | Type declaration for describing an endpoint
+-- reachable at: /:username
+type MyAPI = Capture "username" T.Text 
+          :> Get '[JSON] [Repo]
 
 gitAPI :: Proxy GitRepoAPI
 gitAPI = Proxy
@@ -48,25 +57,35 @@ gitAPI = Proxy
 myAPI :: Proxy MyAPI
 myAPI = Proxy
 
-getRepos :: Maybe Text -> Text
-                       -> Manager 
-                       -> BaseUrl 
-                       -> ExceptT ServantError IO [Repo]
+-- | Function for querying the GitHub API
+getRepos :: Maybe T.Text -> T.Text
+                         -> Manager 
+                         -> BaseUrl 
+                         -> ExceptT ServantError IO [Repo]
 getRepos = client gitAPI
 
-server :: Server MyAPI
-server = pingGitHub 
-  where pingGitHub :: Text -> Handler [Repo]
-        pingGitHub user = liftIO $ do
-          manager <- newManager tlsManagerSettings
-          result  <- runExceptT 
-                   $ getRepos (Just "lucsanszky.github.io") user manager 
-                   $ BaseUrl Https "api.github.com" 443 ""
+sortRepos :: [Repo] -> [Repo]
+sortRepos = sortBy repoSize
 
-          case result of
-          	Right repos -> return repos
-          	Left err    -> return 
-          	             $ [NotFound "https://developer.github.com/v3" "User Not Found"]
+repoSize :: Repo -> Repo -> Ordering
+repoSize (Repo {size = s1}) (Repo {size = s2})
+  | s2 < s1   = LT
+  | otherwise = GT
+
+-- | Given a user handle, it attempts to retrieve
+-- the five largest GitHub repositories of the given user
+-- and list them in descending order
+server :: Server MyAPI
+server user = do
+  manager <- liftIO $ newManager tlsManagerSettings
+  result  <- liftIO $ runExceptT 
+           $ getRepos (Just "lucsanszky") user manager 
+           $ BaseUrl Https "api.github.com" 443 ""
+
+  case result of
+    Right repos -> return $ (take 5) $ sortRepos repos
+    Left err    -> S.throwError 
+                 $ err404 { errBody = "User not found, more info: https://developer.github.com/v3"}
 
 app :: Application
 app = serve myAPI server
