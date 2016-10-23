7eda54b078b5bd6667cc4d5b58d9e943a615bcb5
Daniel Lucsanszky <dan.lucsanszky@gmail.com>
2016-10-23 17:01:46 +0200

    added pretty printing

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Lib
 ( runApp
@@ -9,48 +11,63 @@ module Lib
 
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Except (ExceptT, runExceptT)
-import Data.Aeson
-import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Aeson (ToJSON, FromJSON)
+import Data.Aeson.Encode.Pretty (encodePretty)
+import Data.ByteString.Lazy.Char8 (pack)
 import Data.List (sortBy)
 import Data.Proxy
-import qualified Data.Text as T
+import Data.Text (Text)
 import GHC.Generics
 import Network.HTTP.Client (Manager, newManager)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
-import Network.Wai
-import Network.Wai.Handler.Warp
+import Network.HTTP.Media ((//))
+import Network.Wai 
+import Network.Wai.Handler.Warp (run)
 import qualified Servant as S (throwError)
-import Servant.API
+import Servant.API 
+import Servant.API.ContentTypes (eitherDecodeLenient)
 import Servant.Client
-import Servant.Server
+import Servant.Server (Server, err404, errBody, serve)
 
 -- | Data structure for the JSON data
 data Repo = Repo 
-  { name             :: T.Text
-  , full_name        :: T.Text
+  { name             :: Text
+  , full_name        :: Text
   , size             :: Int
-  , description      :: Maybe T.Text
-  , language         :: Maybe T.Text
-  , created_at       :: T.Text
-  , clone_url        :: T.Text
+  , description      :: Maybe Text
+  , language         :: Maybe Text
+  , created_at       :: Text
+  , clone_url        :: Text
   , stargazers_count :: Int
   } deriving (Show, Generic)
 
+data PrettyJSON
+
 instance ToJSON Repo
-instance FromJSON Repo
+
+instance FromJSON Repo 
+
+instance Accept PrettyJSON where
+    contentType _ = "application" // "json"
+
+instance ToJSON a => MimeRender PrettyJSON a where
+    mimeRender _ = encodePretty
+
+instance FromJSON a => MimeUnrender PrettyJSON a where
+    mimeUnrender _ = eitherDecodeLenient
 
 -- | Type declaration for describing a GitHub API endpoint
 -- reachable at: /users/:username/repos
 type GitRepoAPI = "users"
-               :> Header "User-Agent" T.Text
-               :> Capture "username" T.Text 
+               :> Header "User-Agent" Text
+               :> Capture "username" Text 
                :> "repos" 
-               :> Get '[JSON] [Repo]
+               :> Get '[PrettyJSON] [Repo]
 
 -- | Type declaration for describing an endpoint
 -- reachable at: /:username
-type MyAPI = Capture "username" T.Text 
-          :> Get '[JSON] [Repo]
+type MyAPI = Capture "username" Text 
+          :> Get '[PrettyJSON] [Repo]
 
 gitAPI :: Proxy GitRepoAPI
 gitAPI = Proxy
@@ -59,7 +76,7 @@ myAPI :: Proxy MyAPI
 myAPI = Proxy
 
 -- | Function for querying the GitHub API
-getRepos :: Maybe T.Text -> T.Text
+getRepos :: Maybe Text -> Text
                          -> Manager 
                          -> BaseUrl 
                          -> ExceptT ServantError IO [Repo]
@@ -86,7 +103,7 @@ server user = do
   case result of
     Right repos -> return $ (take 5) $ sortRepos repos
     Left err    -> S.throwError 
-                 $ err404 { errBody = B.pack $ "GitHub Error: " ++ show err}
+                 $ err404 { errBody = pack $ "GitHub Error: " ++ show err}
 
 app :: Application
 app = serve myAPI server
